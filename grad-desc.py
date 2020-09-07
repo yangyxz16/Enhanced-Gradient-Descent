949d1557973a610a7b6b9655ff93658581077375
unknown <yangyxz16@gmail.com>
2020-09-07 14:28:56 -0700

    first commit

---
+++ grad-desc.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 14:23:07 2020
+
+Assignment 1: Enhanced Gradient Descent for UTD CS6375.002 by Professor Anurag Nagar
+
+@author: 
+    Xizhen Yang
+    NetID: XXY180008
+    
+    Churong Zhang
+    NetID:
+"""
+
